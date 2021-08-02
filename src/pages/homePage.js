d9af206d92ea666ded44d2d59c4abd4e28d6480b
John Cagney <59435586+JCagney@users.noreply.github.com>
2021-08-02 14:49:20 +0100

    Implemented pagination for Discover view & api

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -5,10 +5,18 @@ import Spinner from '../components/spinner'
 import {getMovies} from '../api/tmdb-api'
 import AddToFavoritesIcon from '../components/cardIcons/addToFavorites'
 import FavoriteHeader from '../components/cardHeaders/favoriteHeader'
+import Pagination from '@material-ui/lab/Pagination';
 
 
 const HomePage = (props) => {
-  const {  data, error, isLoading, isError }  = useQuery('discover', getMovies)
+  const [page, setPage] = React.useState(1);
+  console.log(page);
+
+
+  const handleChange = (event, value) => {
+    setPage(value);
+  };
+  const {  data, error, isLoading, isError }  = useQuery(["discover", { page: page} ], getMovies);
 
   if (isLoading) {
     return <Spinner />
@@ -16,10 +24,11 @@ const HomePage = (props) => {
 
   if (isError) {
     return <h1>{error.message}</h1>
-  }  
+  }
   const movies = data.results;
 
   return (
+    <>
     <PageTemplate
       title="Discover Movies"
       movies={movies}
@@ -28,10 +37,12 @@ const HomePage = (props) => {
       }}
       cardHeader={(movie) => {
         return <FavoriteHeader movie={movie}/>
-      
+
       }}
       filter={true}
       />
+      <Pagination count={10} page={page} onChange={handleChange} shape="rounded" size="large" />
+      </>
   );
 };
 
